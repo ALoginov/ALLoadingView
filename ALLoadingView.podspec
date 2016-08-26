9cd10b738bca75532ae50aa66ef2b5d6833438ee
ALoginov <artemloginov@me.com>
2016-08-26 21:32:18 +0300

    Updated example project

--- ALLoadingView.podspec
+++ ALLoadingView.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
 s.name             = 'ALLoadingView'
-s.version          = '0.1.3'
+s.version          = '0.1.4'
 s.summary          = 'Manager for loading views presentation'
 
 # This description is used to generate tags and improve search results.
--- Example/ALLoadingView/ViewController.swift
+++ Example/ALLoadingView/ViewController.swift
@@ -1,26 +1,73 @@
-//
-//  ViewController.swift
-//  ALLoadingView
-//
-//  Created by ALoginov on 08/19/2016.
-//  Copyright (c) 2016 ALoginov. All rights reserved.
-//
+/*
+ Copyright (c) 2015 Artem Loginov
+ 
+ Permission is hereby granted,  free of charge,  to any person obtaining a
+ copy of this software and associated documentation files (the "Software"),
+ to deal in the Software without restriction, including without limitation
+ the rights to  use, copy, modify, merge, publish, distribute, sublicense,
+ and/or sell copies of the Software, and to permit persons to whom the
+ Software is furnished to do so, subject to the following conditions:
+ 
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+ 
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ DEALINGS IN THE SOFTWARE.
+ */
 
 import UIKit
 import ALLoadingView
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        
-//        ALLoadingView.manager.messageText = "fuck"
+    private var step = 0
+    private var updateTimer = NSTimer()
+    
+    @IBAction func action_testCaseOne(sender: AnyObject) {
+        ALLoadingView.manager.resetToDefaults()
+        ALLoadingView.manager.showLoadingViewOfType(.Default, windowMode: .Windowed, completionBlock: nil)
+        ALLoadingView.manager.hideLoadingViewWithDelay(2.0)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func action_testCaseTwo(sender: AnyObject) {
+        ALLoadingView.manager.resetToDefaults()
+        ALLoadingView.manager.bluredBackground = true
+        ALLoadingView.manager.showLoadingViewOfType(.MessageWithIndicator, windowMode: .Fullscreen, completionBlock: nil)
+        ALLoadingView.manager.hideLoadingViewWithDelay(2.0)
+    }
+    
+    @IBAction func action_testCaseThree(sender: AnyObject) {
+        ALLoadingView.manager.resetToDefaults()
+        ALLoadingView.manager.bluredBackground = true
+        ALLoadingView.manager.showLoadingViewOfType(.MessageWithIndicatorAndCancelButton, windowMode: .Fullscreen, completionBlock: nil)
+        ALLoadingView.manager.cancelCallback = {
+            ALLoadingView.manager.hideLoadingView()
+        }
+    }
+    
+    @IBAction func action_testCaseFour(sender: AnyObject) {
+        ALLoadingView.manager.resetToDefaults()
+        ALLoadingView.manager.showLoadingViewOfType(.Progress) {
+            (finished) -> Void in
+            ALLoadingView.manager.updateProgressLoadingViewWithMessage("Initializing", forProgress: 0.05)
+            self.step = 1
+            self.updateTimer = NSTimer.scheduledTimerWithTimeInterval(1.0, target: self, selector: #selector(ViewController.updateProgress), userInfo: nil, repeats: true)
+        }
+    }
+    
+    func updateProgress() {
+        let steps = ["Initializing", "Downloading data", "Extracting files", "Parsing data", "Updating database", "Saving"]
+        ALLoadingView.manager.updateProgressLoadingViewWithMessage(steps[step], forProgress: 0.2 * Float(step))
+        step += 1
+        if step == steps.count {
+            ALLoadingView.manager.hideLoadingView()
+            updateTimer.invalidate()
+        }
     }
 }
 
