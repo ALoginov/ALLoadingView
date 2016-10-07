8b24d95df7e7c3c9d24902deb709464796bb0d83
ALoginov <artemloginov@me.com>
2016-10-07 20:45:34 +0300

    #sw3_0003

---
+++ ALLoadingView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ALLoadingView
+//
+//  Created by Artem Loginov on 07.10.16.
+//  Copyright © 2016 ALoginov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
