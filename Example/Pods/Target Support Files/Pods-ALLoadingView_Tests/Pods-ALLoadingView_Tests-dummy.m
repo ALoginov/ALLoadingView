a4d022772ab037343b30d8e8feb2738dc000e626
ALoginov <artemloginov@me.com>
2016-10-07 20:44:24 +0300

    #sw3_0002

--- Example/Pods/Target Support Files/Pods-ALLoadingView_Tests/Pods-ALLoadingView_Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ALLoadingView_Tests : NSObject
-@end
-@implementation PodsDummy_Pods_ALLoadingView_Tests
-@end
--- Example/Pods/Target Support Files/Pods-ALLoadingView_Tests/Pods-ALLoadingView_Tests-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double Pods_ALLoadingView_TestsVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_ALLoadingView_TestsVersionString[];
-
